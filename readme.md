9e7bbf41c1cc391ec1d2b814c3441aed4d0e310f
Michael Ontiveros <onerun@gmail.com>
2016-08-10 12:24:14 -0700

    added body-parser package and GET/:id route

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Express App API To Do
 
-// WRITE YOUR NOTES HERE
+// WRITE NOTES HERE
 
 // What is middleware?
 
@@ -9,4 +9,18 @@
 // How do I check it?
 
 // How do I add custom middlewear to specific routes?
+
+// Why do we move code, export it and require it?
+
+Last line in middleware.js is
+
+    module.exports = middleware
+
+We do this this to make the middleware object defined in middleware.js available outside the module.
+
+// What is requiring a module and then how do we mount it?
+
+'npm install body-parser' & then we require it at the top of server.js.  Then we mount (.use) it.  When we mounted it, we did this...
+
+// Created first GET/todos and tested it in Postman
 
--- server.js
+++ server.js
@@ -3,16 +3,59 @@ var express = require('express')
 // Creating an express app
 var app = express()
 var path = require('path')
+var bodyParser = require('body-parser')
 
 // Use PORT 3000, unless using Heroku port
 var PORT = process.env.PORT || 3000
 var middleware = require('./middleware')
 
-// This middleware is now for the whole app!
+var todos = [
+    {
+        id: 1,
+        description: 'Teach REST API.',
+        completed: false
+    },
+    {
+        id: 2,
+        description: 'Go eat a healthy lunch.',
+        completed: true
+    }
+]
+
+// This middleware module's requireAuthentication
+// function is now for the whole app!
 app.use(middleware.requireAuthentication)
+
+// This entire middleware module is now for whole app!
+// Don't forget parentheses!
+app.use(bodyParser())
 
 app.get('/', function(req, res) {
     res.send('<h1>Express Todo API</h1>')
+})
+
+app.get('/todos', function(req, res) {
+    res.json(todos)
+})
+
+app.get('/todos/:id', function(req, res) {
+
+    // res.send('Asking for todo with id ' + req.params.id)
+
+    // res.json( todos[req.params.id - 1] )
+
+    var todoId = parseInt(req.params.id)
+    var matchedTodo;
+    todos.forEach(function(todo) {
+        if (todoId === todo.id) {
+            matchedTodo = todo;
+        }
+    })
+    if (matchedTodo) {
+        res.json(matchedTodo)
+    } else {
+        res.status(404).send()
+    }
 })
 
 app.get('/about', middleware.logger, function(req, res) {
